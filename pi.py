a7dcc87f6a4cc230f48a65cac935083e50f2a7e2
Kyle Konrad <kyle.t.konrad@gmail.com>
2013-05-16 01:37:38 -0700

    working speech page

--- pi.py
+++ pi.py
@@ -32,6 +32,5 @@ def speech():
   return render_template('speech.html')
 
 
-
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=True)
---
+++ static/js/speech.js
@@ -0,0 +1,124 @@
+showInfo('info_start');
+
+var recognizing = false;
+var ignore_onend;
+var start_timestamp;
+if (!('webkitSpeechRecognition' in window)) {
+  upgrade();
+} else {
+  start_button.style.display = 'inline-block';
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+
+  recognition.onstart = function() {
+    recognizing = true;
+    showInfo('info_speak_now');
+  };
+
+  recognition.onerror = function(event) {
+    if (event.error == 'no-speech') {
+      showInfo('info_no_speech');
+      ignore_onend = true;
+    }
+    if (event.error == 'audio-capture') {
+      showInfo('info_no_microphone');
+      ignore_onend = true;
+    }
+    if (event.error == 'not-allowed') {
+      if (event.timeStamp - start_timestamp < 100) {
+        showInfo('info_blocked');
+      } else {
+        showInfo('info_denied');
+      }
+      ignore_onend = true;
+    }
+  };
+
+  recognition.onend = function() {
+    recognizing = false;
+    if (ignore_onend) {
+      return;
+    }
+    if (!transcript) {
+      showInfo('info_start');
+      return;
+    }
+    showInfo('');
+  };
+
+  recognition.onresult = function(event) {
+    var submit = false;
+    var transcript = '';
+    if (typeof(event.results) == 'undefined') {
+      recognition.onend = null;
+      recognition.stop();
+      upgrade();
+      return;
+    }
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      transcript += event.results[i][0].transcript;
+      submit |= event.results[i].isFinal;
+    }
+    transcript = capitalize(transcript);
+    text.value = linebreak(transcript);
+    if (transcript) {
+      showButtons('inline-block');
+    }
+    if (submit) {
+      form.submit();
+    }
+  };
+}
+
+function upgrade() {
+  start_button.style.visibility = 'hidden';
+  showInfo('info_upgrade');
+}
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+function linebreak(s) {
+  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+var first_char = /\S/;
+function capitalize(s) {
+  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+}
+
+function startButton(event) {
+  if (recognizing) {
+    recognition.stop();
+    return;
+  }
+  recognition.lang = 'en-US'
+  recognition.start();
+  ignore_onend = false;
+  text.value = '';
+  start_img.src = '/intl/en/chrome/assets/common/images/content/mic-slash.gif';
+  showInfo('info_allow');
+  showButtons('none');
+  start_timestamp = event.timeStamp;
+}
+
+function showInfo(s) {
+  if (s) {
+    for (var child = info.firstChild; child; child = child.nextSibling) {
+      if (child.style) {
+        child.style.display = child.id == s ? 'inline' : 'none';
+      }
+    }
+    info.style.visibility = 'visible';
+  } else {
+    info.style.visibility = 'hidden';
+  }
+}
+
+var current_style;
+function showButtons(style) {
+  if (style == current_style) {
+    return;
+  }
+  current_style = style;
+}
