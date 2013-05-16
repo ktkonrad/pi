d935bca7b50e3496b22ef122cfc7446fb404cd83
Kyle Konrad <kyle.t.konrad@gmail.com>
2013-05-16 01:08:31 -0700

    temporary commit for pete

--- pi.py
+++ pi.py
@@ -25,5 +25,13 @@ def voice():
     return "this isn't done yet"
   return render_template('voice.jinja.html')
 
+@app.route('/speech', methods=['GET', 'POST'])
+def speech():
+  if request.method == 'POST':
+    return 'You said ' + request.form['text']
+  return render_template('speech.html')
+
+
+
 if __name__ == "__main__":
-  app.run(debug=True, host='0.0.0.0', port=80, use_reloader=True)
+  app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=True)
